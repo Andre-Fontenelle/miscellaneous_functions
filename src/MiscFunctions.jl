d49657befbb88bbff5b81bde852a89f8765c1194
Andre-Fontenelle <andre.fontas@gmail.com>
2021-07-09 18:06:46 -0300

    de

--- src/MiscFunctions.jl
+++ src/MiscFunctions.jl
@@ -1,5 +1,9 @@
 module MiscFunctions
 
+# ================================= Exports ================================== #
+export absVector
+
+# ================================ Functions ================================= #
 function absVector(matrixInput, dims=1)
     return sqrt.(sum(matrixInput.^2, dims=dims))
 end
