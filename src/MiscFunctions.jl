9db32b677c44c844b3bdaae33e9e0f9abe74b937
Andre-Fontenelle <andre.fontas@gmail.com>
2021-07-09 22:32:50 -0300

    hy

--- src/MiscFunctions.jl
+++ src/MiscFunctions.jl
@@ -4,8 +4,14 @@ module MiscFunctions
 export absVector
 
 # ================================ Functions ================================= #
-function absVector(matrixInput, dims=1)
+function absVector(matrixInput, dims=2)
     return sqrt.(sum(matrixInput.^2, dims=dims))
+end
+
+function colMat(inputArray :: Array{T,1}) where T
+    MatrixOutput = Matrix{T}(undef, length(inputArray), 1)
+    MatrixOutput .= inputArray
+    return MatrixOutput
 end
 
 end # module
